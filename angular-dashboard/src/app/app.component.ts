56333d3aa732de67004ae8d01329fa47776f9d4d
PythonPionier <118120951+PythonPionier@users.noreply.github.com>
2023-12-29 17:27:48 +0100

    erstes säulendiagramm

--- angular-dashboard/src/app/app.component.ts
+++ angular-dashboard/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-dashboard';
+
+
+  chart: any = [];
+ 
+  constructor() {}
+
+  ngOnInit() {
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
 }

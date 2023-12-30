27f979b883d17f95135de536c3c4664142446159
PythonPionier <118120951+PythonPionier@users.noreply.github.com>
2023-12-30 19:55:15 +0100

    erste flexbox scss

--- konjunkturreport/src/app/app.module.ts
+++ konjunkturreport/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IndustrieproduktionComponent } from './industrieproduktion/industrieproduktion.component';
+import { TsplotComponent } from './tsplot/tsplot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndustrieproduktionComponent
+    IndustrieproduktionComponent,
+    TsplotComponent
   ],
   imports: [
     BrowserModule
---
+++ konjunkturreport/src/app/tsplot/tsplot.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tsplot',
+  templateUrl: './tsplot.component.html',
+  styleUrls: ['./tsplot.component.scss']
+})
+export class TsplotComponent {
+
+}
